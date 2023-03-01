d3a5b6eb79879be86a23c269fe7d07f8f968fce2
¨Luraschi <luraschigmatias@hotmail.com>
2023-03-01 00:54:18 -0300

    primer cambio

--- prueba5.js
+++ prueba5.js
@@ -1,7 +1,7 @@
 // Crear funcion login que recibe usuario y contrasena, mostrar mensaje.
 
 var nom = "Gonza";
-var pass = "gonzalito de alberti";
+var pass = "gonzalito de ";
 var permisos = "internet";
 
 function login(nom, pass, permisos) {
